efe7687db617daf89d6d5ce4f10b150b2c711f1b
thiar <thiar.hasbiya@gmail.com>
2015-04-26 01:37:24 +0700

    update route dan template ejs

--- register_user/index.js
+++ register_user/index.js
@@ -1,13 +1,23 @@
 var express = require('express');
 var app = express();
+expressLayouts = require('express-ejs-layouts');
 var bodyParser = require("body-parser");
 var http = require('http').Server(app);
 var mysql = require('mysql');
+var partial = require('express-partial');
 app.use(express.static(__dirname + '/public'));
-
-
-/*route declaration*/
 app.use(bodyParser.urlencoded());
+app.use(partial())
+app.use(expressLayouts)
+app.set('view engine', 'ejs')
+app.set('layout', 'layout')
+/*route declaration*/
+app.get('/',function(req,res){
+	res.render('home', { layout: 'layout' })
+});
+app.get('/tes',function(req,res){
+	res.end("hello");
+})
 
 
 /* Database connection*/
@@ -17,6 +27,7 @@ var connection = mysql.createPool({
   password : '',
   database : 'mydb'
 });
+
  http.listen(3000, function(){
   console.log('listening on *:3000');
 });
