2733595a6334eb9981dbda076b3799258fca1e72
samihd <debepe.10@gmail.com>
2015-04-28 23:01:27 +0700

    nambah

---
+++ backend/lxcjs/apps.js
@@ -0,0 +1,87 @@
+
+var spawn = require('child_process').spawn, 
+    exec = require('child_process').exec;
+
+function clone(vm1, vm2){
+    var c = spawn('lxc-clone',['-0',vm1,'-n',vm2]);
+    c.stdout.on('data', function (data) {
+	console.log('stdout: ' + data);
+    });
+    c.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    c.on('exit', function(code){
+    	if(0 == code){
+	    console.log("lxc-create: "+ vm2);
+	}
+    });
+}
+function start(vm){
+    var c = spawn('lxc-start',['-n',vm,'--daemon']);
+    c.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+    c.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    c.on('exit', function(code){
+        if(0 == code){
+            console.log(vm + " ON");
+        }
+    });
+
+}
+function stop(vm){
+    var c = spawn('lxc-stop',['-n',vm]);
+    c.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+    c.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    c.on('exit', function(code){
+        if(0 == code){
+            console.log(vm + " OFF");
+        }
+    });
+
+}
+
+function delete(vm){
+    var c = spawn('lxc-destroy',['-n',vm]);
+    c.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+    c.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    c.on('exit', function(code){
+        if(0 == code){
+            console.log(vm + " OFF");
+        }
+    });
+
+}
+
+//function config(vm,ip){
+//    var c = exec('cat >> /var/lib/lxc/'+vm+'/config << EOF'+'\n'+
+//	'lxc.network.ipv4 ='+ip+'\n'+
+//	'lxc.network.ipv4.gateway = 10.0.3.1'+'\n'+'EOF');
+//    c.stdout.on('data', function (data) {
+//        console.log('stdout: ' + data);
+//    });
+//    c.stderr.on('data', function (data) {
+//        console.log('stderr: ' + data);
+//    });
+//    c.on('exit', function(code){
+//      if(0 == code){
+//            console.log(vm + " OFF");
+//        }
+//    });
+//}
+
+//clone('vm1', 'vm2');
+//stop('vm1');
+//stop('vm2');
+config('vm1','10.0.3.3');
+start('vm1');
---
+++ backend/lxcjs/lxc.js
@@ -0,0 +1,100 @@
+module.exports = function(config){
+
+    var obj = {}
+    var child = require('child')
+        //sshBind = config.sshBind || false
+
+    //http://stackoverflow.com/questions/10530532/
+    function textToArgs(s){
+        var words = [];
+        s.replace(/"([^"]*)"|'([^']*)'|(\S+)/g,function(g0,g1,g2,g3){ words.push(g1 || g2 || g3 || '')});            
+        return words
+    }
+
+    var sysExec = function(command, onData, onClose){
+
+        onData = onData || function(){}
+        onClose = onClose || function(){}
+
+        if (sshBind != false)
+        {
+            var runCommand = sshBind.slice();
+            runCommand.push(command)
+        } else {
+            var runCommand = textToArgs(command);
+        }
+
+        var errors = '';
+
+        child({
+            command: runCommand.slice(0,1)[0],
+            args: runCommand.slice(1),
+            cbStdout: function(data){ onData(''+data) },
+            cbStderr: function(data){ errors+=data; onData(''+data) },
+            cbClose: function(exitCode){ onClose(exitCode == 0 ? null:exitCode,  errors) }
+        }).start()
+    }
+
+
+    obj.create = function(name, template, config, cbComplete, cbData){
+        sysExec('lxc-create -n '+name+' -t '+template, cbComplete, cbData);
+    }
+    
+    obj.clone = function(name, name2, cbComplete, cbData){
+        sysExec('lxc-create -o '+name+' -n '+name2, cbComplete, cbData);
+    }
+
+    obj.destroy = function(name, cbComplete, cbData){
+        sysExec('lxc-destroy -n '+ name, cbComplete, cbData);
+    }
+
+
+    obj.start = function(name, cbComplete, cbData){
+        sysExec('lxc-start -n '+name+' --daemon ', cbComplete, cbData);
+    }
+
+    obj.stop = function(name, cbComplete, cbData){
+        sysExec('lxc-stop -n '+ name, cbComplete, cbData);
+    }
+
+
+    obj.freeze = function(name, cbComplete, cbData){
+        sysExec('lxc-freeze -n '+name, cbComplete, cbData);
+    }
+    obj.unfreeze = function(name, cbComplete, cbData){
+        sysExec('lxc-unfreeze -n '+name, cbComplete, cbData);
+    }
+
+
+    obj.list = function(cbComplete, cbData){
+
+        var output = '';
+        sysExec('lxc-list', function(data){output+=data}, function(error){
+
+            output = output.split("\n");
+
+            var actual = false;            
+            var result = {
+                running: [],
+                frozen: [],
+                stopped: []
+            }
+
+            for (i in output)
+            {
+                var content = output[i].trim();
+
+                if (content == 'RUNNING' || content == 'FROZEN' || content == 'STOPPED')
+                    actual = content.toLowerCase()
+                else 
+                    if (actual != false && content != '')
+                        result[actual].push(content); 
+            }
+
+            cbData(null, result);
+        });
+    }
+
+
+    return obj;
+}
