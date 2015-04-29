dca52df39de231dafb511fed4ccab866341601b7
samihd <debepe.10@gmail.com>
2015-04-29 08:12:57 +0700

    nambah fucntion install via npm dan apt-get

--- backend/lxcjs/apps.js
+++ backend/lxcjs/apps.js
@@ -62,6 +62,36 @@ function delete(vm){
     });
 
 }
+function installFram(namaFrame, vm){
+    var c = exec('chroot /var/lib/lxc/'+vm+'/rootfs/ npm install -g'+namaFrame);
+    c.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+    c.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    c.on('exit', function(code){
+        if(0 == code){
+            console.log(namaFrame+' terinstall di '+ vm);
+        }
+    });
+}
+
+function installApps(namaApps, vm){
+    var c = exec('chroot /var/lib/lxc/'+vm+'/rootfs/ apt-get --yes --force-yes install '+namaApps);
+    c.stdout.on('data', function (data) {
+        console.log('stdout: ' + data);
+    });
+    c.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+    c.on('exit', function(code){
+        if(0 == code){
+            console.log(namaApps+' terinstall di '+ vm);
+        }
+    });
+}
+
 
 //function config(vm,ip){
 //    var c = exec('cat >> /var/lib/lxc/'+vm+'/config << EOF'+'\n'+
@@ -83,5 +113,5 @@ function delete(vm){
 //clone('vm1', 'vm2');
 //stop('vm1');
 //stop('vm2');
-config('vm1','10.0.3.3');
-start('vm1');
+//config('vm1','10.0.3.3');
+//start('vm1');
