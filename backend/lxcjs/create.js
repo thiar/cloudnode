b5f08fc569d6a87a51d28b204d405e9fc8748be0
samihd <debepe.10@gmail.com>
2015-05-13 01:00:36 +0700

    nambah buat setting ip-perbaikan

--- backend/lxcjs/create.js
+++ backend/lxcjs/create.js
@@ -6,7 +6,7 @@ var spawn = require('child_process').spawn,
 //parameter vm1 diisi dengan 'template'
 //parameter vm2 diisi dengan 'vmnamaUser'
 exports.clone=function(vm1, vm2){
-    var c = spawn('lxc-clone',['-0',vm1,'-n',vm2]);
+    var c = spawn('lxc-clone',['-o',vm1,'-n',vm2]);
     c.stdout.on('data', function (data) {
 	console.log('stdout: ' + data);
     });
@@ -21,8 +21,20 @@ exports.clone=function(vm1, vm2){
 }
 //fungsi start digunakan untuk memulai virtual host yang sudah dibuat
 //parameter vm diisi dengan 'vmnamaUser'
-exports.start=function(vm){
+//untuk konfigurasi ip di lxc-container yang sudah di atur dalam database
+exports.start=function(vm, ip){
+    var f = exec('cat > /var/lib/lxc/'+vm+'/rootfs/etc/network/interfaces << EOF'+'\n'+
+    'auto eth0\n'+
+    'iface eth0 inet static\n'+
+    'address '+ip+'\n'+
+    'netmask 255.255.255.240\n'+
+    'gateway 10.151.36.225'+'\n'+'EOF');
+    f.stderr.on('data', function (data) {
+        console.log('stderr: ' + data);
+    });
+
     var c = spawn('lxc-start',['-n',vm,'--daemon']);
+
     c.stdout.on('data', function (data) {
         console.log('stdout: ' + data);
     });
@@ -88,22 +100,4 @@ exports.destroy=function(vm){
 //    });
 //}
 
-exports.config_ip=function(vm,ip){
-    var c = exec('cat > /var/lib/lxc/'+vm+'/rootfs/etc/network/interfaces << EOF'+'\n'+
-  'auto eth0\n'+
-  'iface eth0 inet static\n'+
-  'address '+ip+'\n'+
-  'netmask 255.255.255.0\n'+
-  'gateway 10.0.3.1'+'\n'+'EOF');
-    c.stdout.on('data', function (data) {
-        console.log('stdout: ' + data);
-    });
-    c.stderr.on('data', function (data) {
-        console.log('stderr: ' + data);
-    });
-   c.on('exit', function(code){
-      if(0 == code){
-            console.log(vm + " OFF");
-        }
-    });
-}
+
